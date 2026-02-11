ad39bea70f0f4ee5738fc5f2004399f1a91eee3c
neon4on <vkam37@gmail.com>
2026-02-11 22:06:07 +0300

    chore(db): switch knex client to mysql2

--- src/db/knex.ts
+++ src/db/knex.ts
@@ -2,16 +2,16 @@ import knex, { Knex } from 'knex';
 import { env } from '../config/env';
 
 export const db: Knex = knex({
-  client: 'pg',
+  client: 'mysql2',
   connection: {
     host: env.db.host,
     user: env.db.user,
     password: env.db.password,
     database: env.db.name,
+    port: 3306,
   },
   pool: {
     min: 2,
     max: 10,
   },
-  searchPath: ['public'],
 });
