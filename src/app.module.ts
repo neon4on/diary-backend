9605b7bb81533d26194bde2658993b83ab3a9c7f
neon4on <vkam37@gmail.com>
2026-03-10 14:02:22 +0300

    ref: import modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { GradesModule } from './grades/grades.module';
+import { ScheduleModule } from './schedule/schedule.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    GradesModule,
+    ScheduleModule,
+  ],
 })
 export class AppModule {}
